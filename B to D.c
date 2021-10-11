82ed0cbb8a80943d724ed5cebf1dc13910d5f951
Megha-Bahuguna <91779683+Megha-Bahuguna@users.noreply.github.com>
2021-10-11 14:08:41 +0530

    initial commit

---
+++ B to D.c
@@ -0,0 +1,21 @@
+// convert binary to decimal no.
+
+#include<stdio.h>
+
+int main()
+{
+
+int d=0, b, base=1, r, n;
+printf("Enter the no. \n");
+scanf("%d",&b);
+ n=b;
+ while(b!=0)
+ {
+ 	r=b%10;
+ 	d=d+(r*base);
+ 	b=b/10;
+ 	base=base*2;
+ }
+ printf(" \ndecimal equal to binary no. is %d \n",d);
+ return 0;
+}
